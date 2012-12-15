fffd5b35ae4e2fa25908266c12d814e4ab58c78c
Douglas Waltman II <doug@dougwaltman.com>
2012-12-15 01:32:16 -0800

    added fonts and fixed bug with shipped alert

--- clientapp/views/main.js
+++ clientapp/views/main.js
@@ -12,14 +12,6 @@ var BaseView = require('views/base'),
     
 module.exports = BaseView.extend({
     initialize: function () {
-        // register some handlers on the "shippedTasks" collections
-        this.model.shippedTasks.on('add', this.handleNewTask, this);
-        this.model.shippedTasks.on('reset', this.handleTasksReset, this);
-
-        // register some handlers on the "shippedTasks" collections
-        this.model.damagingTasks.on('add', this.handleNewTask, this);
-        this.model.damagingTasks.on('reset', this.handleTasksReset, this);
-        
         // when members are reset we want to redraw them all
         this.model.members.on('add', this.handleNewMember, this);
         this.model.members.on('reset', this.handleMembersReset, this);
@@ -34,16 +26,6 @@ module.exports = BaseView.extend({
         this.setElement($('body')[0]);
         // render our app template into the body
         this.$el.html(templates.app());
-    },
-    // shipped tasks just get rendered and prepended to the shipped list
-    // as they are added.
-    handleNewTask: function (model) {
-        this.$('.shippedContainer').prepend(templates.shipped({task: model}));
-    },
-    // If we reset the task list (when it's a new day) we just want to 
-    // clear that html from the DOM.
-    handleTasksReset: function () {
-        this.$('.shippedContainer').empty();
     },
     handleMembersReset: function () {
         // create and append a view for each member
